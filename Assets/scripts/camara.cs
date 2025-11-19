b9b5f36e35bccee3b7c3bfdbd903532254206dc9
Alberto Navas <albertonavasglez@gmail.com>
2025-11-19 17:38:41 +0100

    He actualizado los scripts para que tenga todo, musica, movimiento, etc. También añadí enemigos pero me faltan sus movimientos. El script camara no funciona pero le echaré un vistazo ahora.

---
+++ Assets/scripts/camara.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+
+    public Transform jugador;
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(jugador.position.x, jugador.position.y, transform.position.z);
+    }
+
+}
